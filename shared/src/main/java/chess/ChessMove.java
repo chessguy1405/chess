7653116639c4f1f914bfde5aa2dc69b8f70ef849
Jeremy Nielsen <chessguy1405@gmail.com>
2026-09-05 15:55:10 -0600

    Implemented constructor and all methods of ChessMove.java and added override methods

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents moving a chess piece on a chessboard
  * <p>
@@ -8,22 +10,29 @@ package chess;
  */
 public class ChessMove {
 
+    public ChessPosition startPosition;
+    public ChessPosition endPosition;
+    public ChessPiece.PieceType promotionPiece;
+
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
                      ChessPiece.PieceType promotionPiece) {
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
+        this.promotionPiece = promotionPiece;
     }
 
     /**
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
+        return startPosition;
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
+        return endPosition;
     }
 
     /**
@@ -33,6 +42,32 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+        return promotionPiece;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+    ChessMove that = (ChessMove) obj;
+        return (startPosition.equals(that.startPosition) && endPosition.equals(that.endPosition) && promotionPiece == that.promotionPiece);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 43; // Start with a non-zero, prime number to make order matter
+        result += Objects.hashCode(startPosition);
+        result *= 17; // Multiply by prime number to maintain the integrity of the order
+        result += Objects.hashCode(endPosition);
+        result *= 17;
+        result += Objects.hashCode(promotionPiece);
+
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s-%s",startPosition.toString(),endPosition.toString()); // %s inserts a string value, %n inserts a newline
     }
 }
